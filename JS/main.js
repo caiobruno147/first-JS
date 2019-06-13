16d8ab312cc5a33f2730e479f22d5e842ea11a64
Caio Bruno <caiobruno147@gmail.com>
2019-06-12 21:59:48 -0300

    add

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,40 @@ function soma() {
     //alert(resultado);
     document.querySelector('#resultado').value = resultado;
 }
+function mult() {
+    var numero_1 = +document.querySelector('#campo-1').value;
+    var numero_2 = +document.querySelector('#campo-2').value;
+    var resultado = numero_1 * numero_2;
+    //alert(resultado);
+    document.querySelector('#resultado').value = resultado;
+}
+function div() {
+    var numero_1 = +document.querySelector('#campo-1').value;
+    var numero_2 = +document.querySelector('#campo-2').value;
+    var resultado = numero_1 / numero_2;
+    //alert(resultado);
+    document.querySelector('#resultado').value = resultado;
+}
+function sub() {
+    var numero_1 = +document.querySelector('#campo-1').value;
+    var numero_2 = +document.querySelector('#campo-2').value;
+    var resultado = numero_1 - numero_2;
+    //alert(resultado);
+    document.querySelector('#resultado').value = resultado;
+}
+function modulo() {
+    var numero_1 = document.querySelector('#campo-1').value;
+    var numero_2 = document.querySelector('#campo-2').value;
+    var resultado = Number(numero_1) % Number(numero_2);
+    //alert(resultado);
+    document.querySelector('#resultado').value = resultado;
+}
+function cálcula(sinal) {
+    var resultado= 0;
+    if(sinal == "+"){
+        resultado = numero_1 + numero_2;
+        else if (sinal == "-") {
+            resultado = numero_1 - numero_2;
+        }
+    }
+}
